dd710ea194354cdc936954ab34612e62b6fc5738
Georgy Perepecho <horprogs@gmail.com>
2016-12-07 00:49:18 +0300

    Init

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var http = require('http'),
     fs = require('fs');
 
+var port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
     switch (req.url) {
         case '/send':
             res.end('OK');
             break;
         default:
-            res.end();
+            res.end('Not found');
     }
-}).listen(3000);
+}).listen(port);
